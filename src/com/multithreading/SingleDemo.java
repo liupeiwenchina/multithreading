ca577ede0cb4a9057a2204a64494279b0e4f70e8
liu-peiwen <liupeiwenchina@hotmail.com>
2017-04-11 23:41:49 -0400

    first commit

---
+++ src/com/multithreading/SingleDemo.java
@@ -0,0 +1,26 @@
+package com.multithreading;
+
+//懒汉式
+
+class Single {
+	private static Single s = null;
+	
+	private Single() {}
+	
+	public static Single getInstance() {
+		synchronized(Single.class) {
+			if (s == null) s = new Single();  //存在线程安全隐患
+			return s;
+		}
+	}
+}
+
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
